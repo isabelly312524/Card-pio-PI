d98b50972b19c3a49807f2a81eb93d7c922524e5
isabelly312524 <isabellygiovanabatistadasilva@gmail.com>
2025-09-18 19:43:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="/css/default.css" rel="stylesheet" />
+
+        <title>Larika</title>
+    </head>
+
+    <body>
+
+
+        
+        <nav class="navbar navbar-expand-lg navbar-light">
+
+            <div class="container-fluid">
+
+                <a class="navbar-brand">
+                    <img src="./img/ham-ico-64.png" alt="" width="32" height="32">
+                    Larika
+                </a>
+
+                <button class="navbar-toggler" type="button" 
+                    data-bs-toggle="collapse" data-bs-target="#navbarMain">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarMain">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home Page</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="menuLink"
+                            data-bs-toggle="dropdown">Cardapio</a>
+
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="#">Hamburguer</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+</html>
